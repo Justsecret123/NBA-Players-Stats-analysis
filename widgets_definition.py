34925b375ca3937f42d6a52a4f39578618a0acda
Secret123 <50279622+Justsecret123@users.noreply.github.com>
2021-01-15 01:46:52 +0100

    Separated widgets definition & Data Visualization

--- widgets_definition.py
+++ widgets_definition.py
@@ -3,7 +3,7 @@
 
 # <h1> Libraries </h1>
 
-# In[8]:
+# In[1]:
 
 
 import ipywidgets as widgets
@@ -11,7 +11,7 @@ import ipywidgets as widgets
 
 # <h1> Common widget <h1>
 
-# In[7]:
+# In[2]:
 
 
 def output_widget():
@@ -20,44 +20,21 @@ def output_widget():
 
 # <h1> Heatmap </h1>
 
-# <h2> Button <h2>
-
 # In[3]:
 
 
-def heatmap_button():
+def heatmap_widgets():
     
     heatmap_button = widgets.Button(
         description="Show heatmap",
         button_style="info"
     )
     
-    return heatmap_button
-    
-
-
-# <h2> Checkbox </h2>
-
-# In[9]:
-
-
-def heatmap_checkbox():
-    
-    heatmap_show_annotations = widgets.Checkbox(
+    heatmap_checkbox = widgets.Checkbox(
         value=True,
         description="Show annotations"
     )
-    
-    return heatmap_show_annotations
-
-
-# <h2> Width and Height sliders <h2>
-
-# In[4]:
-
-
-def heatmap_sliders():
-    
+  
     heatmap_width = widgets.IntSlider(
         value=10,
         min=5,
@@ -72,33 +49,23 @@ def heatmap_sliders():
         description="Height"
     )
     
-    return heatmap_width,heatmap_height
+    heatmap_output = widgets.Output()
+    
+    return heatmap_button, heatmap_checkbox, heatmap_width, heatmap_height, heatmap_output 
 
 
-# <h1> Stats: points <h1> 
+# <h1> Points <h1> 
 
-# <h2> Button </h2>
-
-# In[11]:
+# In[4]:
 
 
-def points_button():
+def points_widgets():
     
     points_button = widgets.Button(
     description="Show total points",
     button_style="info"
     )
-    
-    return points_button
 
-
-# <h2> Sliders </h2>
-
-# In[12]:
-
-
-def points_sliders():
-    
     points_width = widgets.IntSlider(
     description="Width",
     value=20,
@@ -113,32 +80,22 @@ def points_sliders():
     max=15
     )
     
-    return points_width,points_height
+    points_output = widgets.Output()
+    
+    return points_button, points_width, points_height, points_output
 
 
 # <h1> Assists </h1>
 
-# <h2> Button </h2>
-
-# In[13]:
+# In[5]:
 
 
-def assists_button():
-    
+def assists_widgets():
+  
     assists_button = widgets.Button(
     description="Show total assists",
     button_style="info"
     )
-    
-    return assists_button
-
-
-# <h2> Sliders </h2>
-
-# In[14]:
-
-
-def assists_sliders():
     
     assists_width = widgets.IntSlider(
     description="Width",
@@ -154,5 +111,131 @@ def assists_sliders():
     max=15
     )
     
-    return assists_width, assists_height
+    assists_output = widgets.Output()
+    
+    return assists_button, assists_width, assists_height, assists_output
+
+
+# <h1> Rebounds </h1>
+
+# In[6]:
+
+
+def rebounds_widgets():
+
+    rebounds_button = widgets.Button(
+    description="Show total rebounds", 
+    button_style="info"
+    )
+    
+    rebounds_width = widgets.IntSlider(
+    description="Width",
+    value=20,
+    min=5,
+    max=20
+    )
+
+    rebounds_height = widgets.IntSlider(
+    description="height",
+    value=5,
+    min=5,
+    max=15
+    )
+    
+    rebounds_output = widgets.Output()
+    
+    return rebounds_button, rebounds_width, rebounds_height, rebounds_output
+
+
+# <h1> Steals </h1>
+
+# In[7]:
+
+
+def steals_widgets():
+    
+    steals_button = widgets.Button(
+        description="Show total steals",
+        button_style="info"
+    )
+    
+    steals_width = widgets.IntSlider(
+        description='Width',
+        value=20,
+        min=5,
+        max=20
+    )
+
+    steals_height = widgets.IntSlider(
+        description="Height",
+        value=5,
+        min=5,
+        max=15
+    )
+    
+    steals_output = widgets.Output()
+    
+    return steals_button, steals_width, steals_height, steals_output
+
+
+# <h1> Blocks </h1>
+
+# In[8]:
+
+
+def blocks_widgets():
+    
+    blocks_button = widgets.Button(
+        description="Show total blocks",
+        button_style="info"
+    )
+
+    blocks_width = widgets.IntSlider(
+        description="Width",
+        value=20,
+        min=5,
+        max=20
+    )
+
+    blocks_height = widgets.IntSlider(
+        description='Height',
+        value=5,
+        min=5,
+        max=15
+    )
+    
+    blocks_output = widgets.Output()
+    
+    return blocks_button, blocks_width, blocks_height, blocks_output
+
+
+# <h1> Totals </h1>
+
+# In[ ]:
+
+
+def totals_widgets():
+
+    totals_button = widgets.Button(
+        description="Show totals",
+        button_style="info"
+    )
+
+    totals_width = widgets.IntSlider(
+        description="Width",
+        value=20,
+        min=5,
+        max=20
+    )
+
+    totals_height = widgets.IntSlider(
+        description="Height",
+        value=5,
+        min=5,
+        max=15
+    )
+
+    totals_output = widgets.Output()
+    
+    return totals_button, totals_width, totals_height, totals_output
 
